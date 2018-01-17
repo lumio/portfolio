1b5afcf53a89767a93cfbc94e1b91aa428df0e3a
Benjamin Falk <benjamin.falk@cyanjoe.com>
2018-01-17 23:54:26 +0100

    Load content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 import * as React from 'react';
+import * as yaml from 'js-yaml';
 
-class App extends React.Component {
+class App extends React.Component<any, any> {
+  constructor( props : any ) {
+    super( props );
+
+    this.state = {
+      pending: true,
+      error: false,
+      data: {},
+    };
+
+    fetch( './content.yml' )
+      .then( ( response ) => response.text() )
+      .then( ( data ) => yaml.safeLoad( data ) )
+      .then( ( result ) => this.setState( {
+        pending: false,
+        data: result,
+      } ) )
+      .catch( ( err ) => {
+        // tslint:disable-next-line
+        console.error( err );
+        this.setState( {
+          pending: false,
+          error: true,
+        } );
+      } );
+  }
+
   render() {
     return (
       <div>
         <h1>Hello world</h1>
+        <pre>{ JSON.stringify( this.state.data, null, 2 ) }</pre>
       </div>
     );
   }
