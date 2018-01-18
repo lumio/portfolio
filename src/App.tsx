ecfd54ebc3123e36ba787ecad3bfc3a202ea022c
Benjamin Falk <benjamin.falk@cyanjoe.com>
2018-01-18 10:25:36 +0100

    Render different states

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,10 @@ class App extends React.Component<any, any> {
       data: {},
     };
 
+    this.loadContent();
+  }
+
+  loadContent() {
     fetch( './content.yml' )
       .then( ( response ) => response.text() )
       .then( ( data ) => yaml.safeLoad( data ) )
@@ -28,13 +32,37 @@ class App extends React.Component<any, any> {
       } );
   }
 
-  render() {
+  renderPending() {
+    return (
+      <div>Pending</div>
+    );
+  }
+
+  renderError() {
+    return (
+      <div><strong>error</strong></div>
+    );
+  }
+
+  renderPortfolio() {
     return (
       <div>
         <h1>Hello world</h1>
         <pre>{ JSON.stringify( this.state.data, null, 2 ) }</pre>
       </div>
     );
+  }
+
+  render() {
+    if ( this.state.pending ) {
+      return this.renderPending();
+    }
+
+    if ( this.state.error || !this.state.data ) {
+      return this.renderError();
+    }
+
+    return this.renderPortfolio();
   }
 }
 
