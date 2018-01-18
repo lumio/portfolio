b8f1697fa634fb7901137704ddbc1c04d39c1c66
Benjamin Falk <benjamin.falk@cyanjoe.com>
2018-01-18 12:42:08 +0100

    Test if snapshots render on either received data or on errors

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,46 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+
 import App from './App';
 
-it( 'renders without crashing', () => {
-  const div = document.createElement( 'div' );
-  ReactDOM.render( <App />, div );
+const mockContent = `---
+titlescreen:
+  title: Portfolio
+  subtitle: of Henry Ford
+  mail: henry [dot] ford [at] example [dot] com
+`
+
+const mockResponse = ( status, statusText, response ) => {
+  return new window.Response( response, {
+    status,
+    statusText,
+    headers: {
+      'Content-type': 'text/yaml',
+    },
+  } );
+};
+
+describe( 'App', () => {
+  it( 'renders snapshot on data', ( done ) => {
+    fetch.mockResponse( mockContent );
+
+    const renderedApp = renderer.create( <App /> );
+    expect( renderedApp.toJSON() ).toMatchSnapshot();
+    setTimeout( () => {
+      expect( renderedApp.toJSON() ).toMatchSnapshot();
+      done();
+    }, 100 );
+  } );
+
+  it( 'renders snapshot on error', ( done ) => {
+    fetch.mockReject( new Error( 'some error' ) );
+
+    const renderedApp = renderer.create( <App /> );
+    expect( renderedApp.toJSON() ).toMatchSnapshot();
+    setTimeout( () => {
+      expect( renderedApp.toJSON() ).toMatchSnapshot();
+      done();
+    }, 100 );
+  } );
 } );
