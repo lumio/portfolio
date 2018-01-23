f6bbf3534b3b67fca399d31eda123db16f85686d
Benjamin Falk <benjamin.falk@cyanjoe.com>
2018-01-23 18:12:22 +0100

    Set global font size and default element styles

--- src/globals/styles/globalStyles.ts
+++ src/globals/styles/globalStyles.ts
@@ -8,9 +8,27 @@ export default injectGlobal`
     box-sizing: border-box;
   }
 
+  :root {
+      font-size: 62.5%;
+  }
+
   body {
-    font: 1rem/1.3 sans-serif;
+    font: 1.4rem/1.3 Raleway, sans-serif;
+    font-variant-ligatures: common-ligatures;
     background: url( ${ images.backgroundImage } ) no-repeat fixed left bottom;
     background-size: cover;
+  }
+
+  a, a:link, a:visited {
+    display: inline-block;
+    color: #000;
+    padding: .25rem .5rem;
+    margin: -.25rem -.5rem;
+  }
+
+  a:hover {
+    color: #fff;
+    background: #000;
+    text-decoration: none;
   }
 `;
