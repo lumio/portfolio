d1ff04ef97e3c37059c8a0f24c0bc8442859d3f2
Benjamin Falk <benjamin.falk@cyanjoe.com>
2018-02-08 22:30:34 +0100

    #5 Add basic dubf showcase

--- src/components/Device/devices/index.ts
+++ src/components/Device/devices/index.ts
@@ -1,8 +1,12 @@
+import DubfShowcaseBrowser from './DubfShowcaseBrowser';
+import DubfShowcasePhone from './DubfShowcasePhone';
 import SchweizerIllustrierteShowcaseBrowser from './SchweizerIllustrierteShowcaseBrowser';
 import SchweizerIllustrierteShowcasePhone from './SchweizerIllustrierteShowcasePhone';
 import WallboardShowcase from './WallboardShowcase';
 
 export default {
+  DubfShowcaseBrowser,
+  DubfShowcasePhone,
   SchweizerIllustrierteShowcaseBrowser,
   SchweizerIllustrierteShowcasePhone,
   WallboardShowcase,
