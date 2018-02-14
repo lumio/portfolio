a72c36a4c9b01e266689c409330b3ac4fbf1b3b4
Benjamin Falk <benjamin.falk@cyanjoe.com>
2018-02-14 17:51:45 +0100

    #9 Fix dubf phone showcase for mobile devices

--- src/components/Device/devices/DubfShowcasePhone/styles.ts
+++ src/components/Device/devices/DubfShowcasePhone/styles.ts
@@ -5,38 +5,62 @@ const DubfShowcasePhoneStyles = styled.div`
   ${ ( props : DubfShowcasePhoneStylesPropsType ) => '' }
   width: 100%;
   height: 100%;
-  padding: 3rem 2rem 2rem;
+  padding: 1rem .5rem .5rem;
+  font-size: 8px;
+  line-height: 1;
 
   .dubf__intro {
     text-align: center;
     font-weight: 600;
-    margin-bottom: 1rem;
+    margin-bottom: .5rem;
+
+    ${ ( props ) => props.theme.media.tablet } {
+      margin-bottom: 1rem;
+    }
   }
 
   .dubf__option-container {
     box-shadow: ${ ( props ) => props.isInView ?
-      '1.5rem 1.5rem 0 rgba( 0, 0, 0, .2 )' : 'none' };
+      '.3rem .3rem 0 rgba( 0, 0, 0, .2 )' : 'none' };
     transform:
       ${ ( props ) => props.isInView ?
-        'none' : 'translate( 1.5rem, 1.5rem )' };
+        'none' : 'translate( .3rem, .3rem )' };
     width: 100%;
     transition: box-shadow 1s ease, transform 1s ease;
+
+    ${ ( props ) => props.theme.media.tablet } {
+      box-shadow: ${ ( props ) => props.isInView ?
+        '1.5rem 1.5rem 0 rgba( 0, 0, 0, .2 )' : 'none' };
+      transform:
+        ${ ( props ) => props.isInView ?
+          'none' : 'translate( 1.5rem, 1.5rem )' };
+    }
   }
 
   .dubf__option {
     display: flex;
     justify-content: center;
     align-items: center;
-    min-height: 8rem;
+    min-height: 3.2rem;
 
     color: #fff;
     background: #e30e20;
     font-weight: 600;
     text-shadow: .2rem .2rem 0 #000;
+
+    ${ ( props ) => props.theme.media.tablet } {
+      min-height: 8rem;
+    }
   }
 
   .dubf__option:last-child {
     background: #d60d1f;
+  }
+
+  ${ ( props ) => props.theme.media.tablet } {
+    font-size: 1.6rem;
+    line-height: 1.3;
+    padding: 3rem 2rem 2rem;
   }
 `;
 
